2d05ac5bd9cd349aa1d7a6e48f9c7551613bf348
Daniel Schramm <daniel.schramm@emvi.com>
2021-04-01 23:13:25 +0200

    Added theme switch.

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,8 +1,42 @@
-function enableDarkMode() {
-    document.documentElement.classList.add("theme-dark");
-    document.documentElement.classList.remove("theme-light");
-}
+document.addEventListener('DOMContentLoaded', function() {
+    let darkMode = localStorage.getItem("darkMode");
 
-if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    enableDarkMode();
-}
+    if(darkMode === null && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        enableDarkMode();
+    }
+
+    function enableDarkMode() {
+        document.documentElement.classList.add("theme-dark");
+        document.documentElement.classList.remove("theme-light");
+        localStorage.setItem("darkMode", "enabled");
+    }
+
+    function disableDarkMode() {
+        document.documentElement.classList.add("theme-light");
+        document.documentElement.classList.remove("theme-dark");
+        localStorage.setItem('darkMode', null);
+    }
+
+    if(darkMode === "enabled") {
+        enableDarkMode();
+    }
+
+    const darkModeToggle = document.querySelector("#darkModeToggle");
+
+    if(darkModeToggle) {
+        darkModeToggle.addEventListener("click", e => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            console.log("test")
+            
+            darkMode = localStorage.getItem("darkMode"); 
+            
+            if (darkMode !== "enabled") {
+                enableDarkMode();
+            } else {  
+                disableDarkMode(); 
+            }
+        });
+    }
+}, false);
